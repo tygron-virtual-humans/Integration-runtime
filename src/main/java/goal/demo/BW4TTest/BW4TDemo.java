f26b60da98fe6b891106e72444dc7a478a92aa00
berndkr <bernd.kreynen@hotmail.com>
2015-05-21 09:18:53 +0200

    added the possibility to change the default goal utility through the emotionconfig

---
+++ src/main/java/goal/demo/BW4TTest/BW4TDemo.java
@@ -0,0 +1,24 @@
+package goal.demo.BW4TTest;
+
+import goal.tools.SingleRun;
+import goal.tools.errorhandling.exceptions.GOALRunFailedException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import krTools.errors.exceptions.ParserException;
+import languageTools.exceptions.relationParser.InvalidEmotionConfigFile;
+import languageTools.parser.relationParser.EmotionConfig;
+
+
+public class BW4TDemo {
+
+	public static void main(String[] args) throws ParserException, GOALRunFailedException, FileNotFoundException, InvalidEmotionConfigFile {
+
+		SingleRun run = new SingleRun(new File("src/main/java/goal/demo/BW4TTest/kreynen_spanoghe.mas2g"), 100000); 
+		EmotionConfig.parse("src/main/java/goal/demo/BW4TTest/emotionConfig");
+	    run.run();
+
+	}
+
+}
