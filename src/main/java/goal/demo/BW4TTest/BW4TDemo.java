cb1450559353b65c31c2ca1b1a97f972c2000c61
berndkr <bernd.kreynen@hotmail.com>
2015-05-23 14:00:07 +0200

    changed POM to use new version 1.2.0 of grammar.

--- src/main/java/goal/demo/BW4TTest/BW4TDemo.java
+++ src/main/java/goal/demo/BW4TTest/BW4TDemo.java
@@ -14,10 +14,8 @@ import languageTools.parser.relationParser.EmotionConfig;
 public class BW4TDemo {
 
 	public static void main(String[] args) throws ParserException, GOALRunFailedException, FileNotFoundException, InvalidEmotionConfigFile {
-
 		SingleRun run = new SingleRun(new File("src/main/java/goal/demo/BW4TTest/kreynen_spanoghe.mas2g"), 100000); 
 	    run.run();
-
 	}
 
 }
