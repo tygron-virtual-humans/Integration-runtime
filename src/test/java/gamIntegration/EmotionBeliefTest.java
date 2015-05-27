71e25d85401a725c7323a82658b0ccdf2a5b8947
berndkr <bernd.kreynen@hotmail.com>
2015-05-27 10:33:32 +0200

    fixing minimal test

--- src/test/java/gamIntegration/EmotionBeliefTest.java
+++ src/test/java/gamIntegration/EmotionBeliefTest.java
@@ -1,7 +1,10 @@
 package gamIntegration;
 
+import languageTools.parser.relationParser.EmotionConfig;
+
 import org.junit.Test;
 
+import goal.core.gamygdala.Engine;
 import goal.parser.unittest.AbstractUnitTestTest;
 import goal.tools.unittest.result.UnitTestResult;
 
@@ -13,7 +16,8 @@ public class EmotionBeliefTest extends AbstractUnitTestTest {
 		//Engine gamEngine = Engine.getInstance();
 		//gamEngine.setStart(true);
 		//gamEngine.useFile(true);
-		
+		Engine.getInstance().reset();
+		EmotionConfig.getInstance().reset();
 		UnitTestResult results = runTest("src/test/resources/goal/parser/unittest/CountsTo100Test2.test2g");
 		assertPassedAndPrint(results);
 		//testGamGoals.test2g");
