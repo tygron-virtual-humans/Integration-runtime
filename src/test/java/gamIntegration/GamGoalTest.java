471837f023845afce589ee7f3095c84d7d9e1a6a
berndkr <bernd.kreynen@hotmail.com>
2015-06-05 13:31:46 +0200

    Fixed tests

--- src/test/java/gamIntegration/GamGoalTest.java
+++ src/test/java/gamIntegration/GamGoalTest.java
@@ -18,10 +18,9 @@ public class GamGoalTest extends AbstractUnitTestTest {
 		//gamEngine.useFile(true);
 		
 		runTest("src/test/resources/goal/parser/unittest/testGamGoals.test2g");
-
-		assertTrue(gamEngine.getMap().getGoalMap().containsKey("aap/1"));
-		assertEquals(gamEngine.getMap().getAgentMap().getAgentByName("agentUnderTest").getGoalByName("aap/1").getName(), "aap/1");
-		assertEquals(gamEngine.getMap().getAgentMap().getAgentByName("secondAgentUnderTest").getGoalByName("aap/1").getName(), "aap/1");
+		assertTrue(gamEngine.getMap().getGoalMap().containsKey("aap(12)"));
+		assertEquals(gamEngine.getMap().getAgentMap().getAgentByName("agentUnderTest").getGoalByName("aap(12)").getName(), "aap(12)");
+		assertEquals(gamEngine.getMap().getAgentMap().getAgentByName("secondAgentUnderTest").getGoalByName("aap(12)").getName(), "aap(12)");
 
 		//gamEngine.reset();
 	}
