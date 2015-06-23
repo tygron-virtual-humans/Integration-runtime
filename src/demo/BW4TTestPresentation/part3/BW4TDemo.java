c58a57ae336f8909b43748baf045ed61b956b567
berndkr <bernd.kreynen@hotmail.com>
2015-06-23 10:19:40 +0200

    files for demo during meeting

---
+++ src/demo/BW4TTestPresentation/part3/BW4TDemo.java
@@ -0,0 +1,22 @@
+package BW4TTestPresentation.part3;
+
+import goal.tools.SingleRun;
+import goal.tools.errorhandling.exceptions.GOALRunFailedException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import krTools.errors.exceptions.ParserException;
+import languageTools.exceptions.relationParser.InvalidEmotionConfigFile;
+import languageTools.parser.relationParser.EmotionConfig;
+
+
+public class BW4TDemo {
+
+	public static void main(String[] args) throws ParserException, GOALRunFailedException, FileNotFoundException, InvalidEmotionConfigFile {
+		SingleRun run = new SingleRun(new File("src/demo/BW4TTestPresentation/part3/kreynen_spanoghe.mas2g"), 100000); 
+	    run.run();
+	}
+
+}
+
